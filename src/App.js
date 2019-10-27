7ea2829be16c3694ef0f781b61b0521a0bf190b9
BlackAce <alfredo.gon195@gmail.com>
2019-10-27 00:33:35 -0700

    added state and next to work on wrapper and render

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
-import React, { component } from "react";
+import React, { Component } from "react";
+import characterChard from "./components/characterCard"
+import characters from "./characters.json";
+
+
+class App extends Component {
+    state = {
+        characters
+    }
+}
+
+
+export default App;
