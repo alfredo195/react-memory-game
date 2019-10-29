b44cfe05de7f34e08370d89d328e8c88d5924f5b
BlackAce <alfredo.gon195@gmail.com>
2019-10-28 22:56:46 -0700

    added nav bar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+
+const NavTab = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <Navbar color="dark" dark expand="md">  
+            <NavbarBrand href="/">Clicky Game</NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink href="https://github.com/alfredo195" target="_blank" active>GitHub</NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
+    );
+}
+
+export default NavTab;
