9323fdfd8a6c3995b568435d0506026655c89773
BlackAce <alfredo.gon195@gmail.com>
2019-10-29 00:04:57 -0700

    Updates

---
+++ precache-manifest.f59a7e17765602b5e7f736d210fc01ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29b3cadc60384ad84228331f7397d70",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "9557dcd766e24d8dc779",
+    "url": "/react-memory-game/static/css/main.cc9637ae.chunk.css"
+  },
+  {
+    "revision": "60e5ab0427f0b006bd79",
+    "url": "/react-memory-game/static/js/2.387054ee.chunk.js"
+  },
+  {
+    "revision": "9557dcd766e24d8dc779",
+    "url": "/react-memory-game/static/js/main.607711d3.chunk.js"
+  },
+  {
+    "revision": "e7cce8f96413fdbde7a2",
+    "url": "/react-memory-game/static/js/runtime-main.b1273a96.js"
+  }
+]);
